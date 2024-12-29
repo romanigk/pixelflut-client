607b296ba523c5d9d9f812def1f035d79e8d67ad
Robert Manigk <p1ng0ut@mailbox.org>
2024-12-29 15:57:31 +0100

    Remove obsolete HelloTest.kt file
    The HelloTest.kt file was no longer relevant or in use. Its removal cleans up the codebase and eliminates a redundant testing artifact.

--- src/test/kotlin/HelloTest.kt
+++
@@ -1,9 +0,0 @@
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
-
-class HelloTest : StringSpec({
-    "greets the right person" {
-        // a great test...
-        greet() shouldBe "Hello, World!"
-    }
-})
