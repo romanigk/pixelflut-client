b8bd475cebedfcc7d4d951653d506aa9bfa14b2f
Robert Manigk <p1ng0ut@mailbox.org>
2024-12-29 15:11:45 +0100

    Update file reading logic to use classloader for resources
    The file reading approach is changed to utilize the classloader, ensuring compatibility with resources bundled within the application. This prevents file-not-found issues when accessing files from the resources directory. An exception is now thrown if the file is not located.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,25 @@
-import java.io.File
-
 fun main() {
     val host = "151.217.60.77"
     val port = 1337
     // Read dezentrale2.png from resources folder
-    val pixelGraphic = readPixelGraphicFromFile("resources/dezentrale2.png")
+    val pixelGraphic = readPixelGraphicFromFile("dezentrale2.png")
     while (true) {
         sendPixelGraphicViaTelnet(host, port, pixelGraphic)
     }
 }
 
 fun readPixelGraphicFromFile(filePath: String): Array<Array<Int>> =
-    File(filePath)
-        .readLines()
-        .map { line ->
-            line.split(",").map { it.trim().toInt() }.toTypedArray()
-        }.toTypedArray()
+    object {}
+        .javaClass.classLoader
+        .getResource(filePath)
+        ?.readText()
+        ?.lineSequence()
+        ?.map { line ->
+            line
+                .split(",")
+                .map { it.trim().toInt() }
+                .toList()
+                .toTypedArray()
+        }?.toList()
+        ?.toTypedArray()
+        ?: throw IllegalArgumentException("File not found: $filePath")
