e8186f73fe98afc39a0ffb4b580e6f0179de8f0e
Robert Manigk <p1ng0ut@mailbox.org>
2024-12-29 23:07:35 +0100

    Refactor pixel graphic loading logic
    Replaced `loadPixelGraphic` with a new `loadPixelGraphics` function for improved clarity and maintainability. Introduced `RGB_MASK` constant and simplified code structure for better readability and reusability.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,32 @@ import javax.imageio.ImageIO
 
 const val HOST = "151.217.60.77"
 const val PORT = 1337
-const val FILE_PATH = "dezentrale2.png"
+const val RGB_MASK = 0xFFFFFF // Maske zur Extraktion von RGB-Werten (24-Bit)
 
 fun main() {
-    val pixelGraphic = loadPixelGraphic(FILE_PATH)
+    val filePath = "dezentrale2.png" // Bilddatei
+    val pixelGraphic = loadPixelGraphics(filePath)
     processPixelGraphic(HOST, PORT, pixelGraphic)
+}
+
+fun loadPixelGraphics(imagePath: String): Array<Array<Int>> {
+    val image = loadBufferedImage(imagePath)
+    val pixelData = image.getRGB(0, 0, image.width, image.height, null, 0, image.width)
+    return Array(image.height) { y ->
+        Array(image.width) { x ->
+            pixelData[y * image.width + x] and RGB_MASK
+        }
+    }
+}
+
+private fun loadBufferedImage(imagePath: String): BufferedImage {
+    val filePath =
+        object {}
+            .javaClass.classLoader
+            .getResource(imagePath)
+            ?.file
+            ?: throw IllegalArgumentException("File not found: $imagePath")
+    return ImageIO.read(File(filePath))
 }
 
 fun processPixelGraphic(
@@ -19,24 +40,5 @@ fun processPixelGraphic(
     sendHelpCommandViaTelnet(host, port)
     while (true) {
         sendPixelGraphicViaTelnet(host, port, pixelGraphic)
-    }
-}
-
-fun loadPixelGraphic(filePath: String): Array<Array<Int>> {
-    // Lade die Bilddatei
-    val file =
-        object {}
-            .javaClass.classLoader
-            .getResource(filePath)
-            ?.file
-            ?: throw IllegalArgumentException("File not found: $filePath")
-    val image: BufferedImage = ImageIO.read(File(file))
-
-    // Verwende getRGB, um die Pixelmappe direkt zu erstellen
-    val pixelData = image.getRGB(0, 0, image.width, image.height, null, 0, image.width)
-    return Array(image.height) { y ->
-        Array(image.width) { x ->
-            pixelData[y * image.width + x] and 0xFFFFFF // Extrahiere RGB-Wert (24-Bit)
-        }
     }
 }
